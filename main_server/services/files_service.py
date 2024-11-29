1d93b609873beb3eef7af326553c481a2cf5f77a
vqlion <valentin.jossic@insa-lyon.fr>
2024-11-29 11:53:00 +0100

    Build hash table and map filenames to ip addresses

---
+++ main_server/services/files_service.py
@@ -0,0 +1,54 @@
+from os import listdir
+from os.path import isfile, join
+import hashlib
+
+h = hashlib.md5()
+digest_size_bit = h.digest_size*8
+
+dir_path = '/home/val/Documents/5TC/CDN/CDN-project/main_server/contents'
+
+def list_files(path):
+    files_list = [f for f in listdir(path) if isfile(join(path, f))]
+    return files_list
+
+def hash_filenames(files_list):
+    list_hashes = []
+    for filename in files_list:
+        h = hashlib.md5()
+        h.update(bytes(filename, 'utf-8'))
+        filename_hash = int(h.hexdigest(), 16)
+
+        list_hashes.append({ "filename": filename, "hash": filename_hash })
+
+    return list_hashes
+
+def build_hash_table(ip_list):
+    hash_table = []
+
+    for i, ip in enumerate(ip_list):
+        hash_table.append({
+            "hash": int((2**digest_size_bit - 1) * ((i + 1) / len(ip_list))),
+            "ip": ip
+        })
+
+    return hash_table
+
+def map_filenames_to_ips(hash_filenames, hash_table):
+    filename_ips = []
+    for filename_hash in hash_filenames:
+        for entry in hash_table:
+            if filename_hash["hash"] < entry["hash"]:
+                filename_ips.append({
+                    "filename": filename_hash["filename"],
+                    "ip": entry["ip"]
+                })
+                break
+    return filename_ips
+
+files_list = list_files(dir_path)
+files_hashes = hash_filenames(files_list)
+
+hash_table = build_hash_table(["1.1.1.1", "2.2.2.2", "3.3.3.3"])
+print(files_hashes)
+print(hash_table)
+print(map_filenames_to_ips(files_hashes, hash_table))
