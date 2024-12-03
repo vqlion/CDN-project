250e90fe121a3c2c9b09346df356e2ca90717dc0
vqlion <valentin.jossic@insa-lyon.fr>
2024-12-03 15:33:12 +0100

    Files and hash table distribution logic

---
+++ replica_server/api/peer/source_communications.py
@@ -0,0 +1,29 @@
+from flask import send_from_directory, request, Response
+import os.path
+from dotenv import load_dotenv
+import requests
+import re
+import json
+
+load_dotenv()
+
+STATIC_FOLDER = os.getenv('FILES_FOLDER')
+DEFAULT_FILE_PATH = os.getenv('DEFAULT_FILE_PATH')
+CENTRAL_SERVER_ADDRESS = os.getenv('CENTRAL_SERVER_ADDRESS')
+
+def init(app):
+
+    @app.route('/upload', methods=["PUT"])
+    def upload_files():
+        if request.files:
+            files_list = request.files.values()
+            for file in files_list:
+                file.save(os.path.join('./replica_server/static', file.filename))
+            return Response("", 200)
+        return Response("", 500)
+        
+    @app.route('/upload-hash-table', methods=["PUT"])
+    def upload_hash_table():
+        with open("hash-table.json", "w") as json_file:
+            json.dump(request.json, json_file)
+        return Response("", 200)
