7a1a541a155a8819091939ef47f1131b215cd6fa
EoliaBagnoli <eolia.bagnoli@insa-lyon.fr>
2024-12-13 16:23:48 +0100

    final changes before first test w/ gns3

--- replica_server/main.py
+++ replica_server/main.py
@@ -1,5 +1,5 @@
 from LRU_cache import LRUCache
-from flask import Flask, send_file, redirect
+from flask import Flask, send_file, redirect, request, make_response
 import os.path
 
 # creates a Flask application 
@@ -8,15 +8,19 @@ cache = LRUCache()
 
 @app.route("/ask_cache/<filename>") 
 def fetch_file(filename) :
+    src_addr = request.remote_addr
+    print(f"REMOTE ADDRESSSSSS : {src_addr}")
     cache.get_file(filename)
 
     file_path = "contents/" + filename
-    default_file_path = "default.png"
+    default_file_path = "contents/default.png"
     exists = os.path.isfile(file_path)
     if exists :
         return send_file(file_path) 
-    return redirect(default_file_path)
+    default_response = make_response(send_file(default_file_path))
+    default_response.headers['X-Custom-Filename'] = "default.png"
+    return default_response
 
 # run the application 
 if __name__ == "__main__" :
-	app.run(host="0.0.0.0", debug=True)
+	app.run(host="0.0.0.0", port=5001, debug=True)
